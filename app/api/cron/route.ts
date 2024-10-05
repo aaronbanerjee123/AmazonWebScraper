9effcc8a04b43da1b3b366b3f6132e61d976e6f4
aaronbanerjee123 <aaronbaner@outlook.com>
2024-10-05 15:40:44 -0400

    added new params

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,12 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300;
+
+export const dynamic = 'force-dynamic';
+
+export const revalidate = 0;
+
 export const GET = async () => {
   try {
     connectToDB();
